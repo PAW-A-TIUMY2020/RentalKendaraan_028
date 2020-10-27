1bae05e2a2097746e5791591f61fa0ac1d1e81e6
Khumairi Zulhizmarch <khumairiari21@gmail.com>
2020-10-27 22:49:58 +0700

    Add project files.

---
+++ RentalKendaraan_028/Models/Costumer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RentalKendaraan_028.Models
+{
+    public partial class Costumer
+    {
+        public Costumer()
+        {
+            Peminjaman = new HashSet<Peminjaman>();
+        }
+
+        public int IdCustomer { get; set; }
+        public string NamaCustomer { get; set; }
+        public string Nik { get; set; }
+        public string Alamat { get; set; }
+        public string NoHp { get; set; }
+        public int? IdGender { get; set; }
+
+        public Gender IdGenderNavigation { get; set; }
+        public ICollection<Peminjaman> Peminjaman { get; set; }
+    }
+}
---
+++ RentalKendaraan_028/Models/Peminjaman.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RentalKendaraan_028.Models
+{
+    public partial class Peminjaman
+    {
+        public int IdPeminjaman { get; set; }
+        public DateTime? TglPeminjaman { get; set; }
+        public int? IdKendaraan { get; set; }
+        public int? IdCustomer { get; set; }
+        public int? IdJaminan { get; set; }
+        public int? Biaya { get; set; }
+
+        public Costumer IdCustomerNavigation { get; set; }
+        public Jaminan IdJaminanNavigation { get; set; }
+        public Kendaraan IdKendaraanNavigation { get; set; }
+        public Pengembalian IdPeminjamanNavigation { get; set; }
+    }
+}
